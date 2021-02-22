f1fddffb9daa5f9e4c32aae93b57d80a1f464333
fernandamello <fernanda.mello02@edu.pucrs.br>
2021-02-22 18:42:29 -0300

    Primeiro dia do NLW e componente ExperienceBar

---
+++ moveit/src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css'
+
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>O xp</span>
+            <div>
+                <div style={{ width: '50%' }}/>
+                <span className="current-experience" style={{ left: '50%'}}>
+                    300 xp
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+}
